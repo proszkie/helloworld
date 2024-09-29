5bd7a417b213588d5834a26f1710e90ff9d47629
Piotr Proszowski <p.proszowski@gmail.com>
2024-09-29 16:44:33 +0200

    Add axion

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,18 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
+
+	id("pl.allegro.tech.build.axion-release") version "1.17.2"
 }
 
+
 group = "dev.proszowski"
-version = "0.0.1-SNAPSHOT"
+
+project.version = scmVersion.version
+
+scmVersion {
+	versionIncrementer("incrementMinor")
+}
 
 java {
 	toolchain {
